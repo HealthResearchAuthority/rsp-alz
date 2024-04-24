fdda9a90431be87c42139f14a2c399d5161cb742
Nikhil Bharathesh <nikhil.bharathesh_pa@hra.nhs.uk>
2024-04-24 15:09:18 +0100

    only deploy to development

--- pipeline-scripts/Deploy-ALZLoggingAndSentinelResourceGroup.ps1
+++ pipeline-scripts/Deploy-ALZLoggingAndSentinelResourceGroup.ps1
@@ -12,7 +12,7 @@ param (
   [Boolean]$WhatIfEnabled = [System.Convert]::ToBoolean($($env:IS_PULL_REQUEST)),
 
   [Parameter()]
-  [String[]] $SubscriptionIds = @('hra-rsp-dev', 'hra-rsp-systemtest-manual', 'hra-rsp-systemtest-automated', 'hra-rsp-system-integration')
+  [String[]] $SubscriptionIds = @('b83b4631-b51b-4961-86a1-295f539c826b') #Starting with Development environment
 )
 
 # Parameters necessary for deployment
