5ac1c96b2e14b5f810ec4fca3b8ae50f4c840ef5
Nikhil Bharathesh <nikhil.bharathesh_pa@hra.nhs.uk>
2024-04-25 15:19:16 +0100

    updated subscriptionID for logging and diagnostic

--- pipeline-scripts/Deploy-ALZMGDiagnosticSettings.ps1
+++ pipeline-scripts/Deploy-ALZMGDiagnosticSettings.ps1
@@ -6,7 +6,7 @@ param (
   [String]$TopLevelMGPrefix = "$($env:TOP_LEVEL_MG_PREFIX)",
 
   [Parameter()]
-  [String]$ManagementSubscriptionId = "$($env:MANAGEMENT_SUBSCRIPTION_ID)",
+  [String]$ManagementSubscriptionId = "$($env:LOGGING_DIAGNOSTICS_SUBSCRIPTION_ID)", #use existing Workspace under Dev-Box subscription
 
   [Parameter()]
   [String]$TemplateFile = "upstream-releases\$($env:UPSTREAM_RELEASE_VERSION)\infra-as-code\bicep\orchestration\mgDiagSettingsAll\mgDiagSettingsAll.bicep",
