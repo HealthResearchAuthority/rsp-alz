d00ecc25c35fafdba351ab66f4256153f53645f1
Nikhil Bharathesh <nikhil.bharathesh_pa@hra.nhs.uk>
2024-05-15 11:14:52 +0100

    fixing deployment script

--- pipeline-scripts/Deploy-ALZHub-lzVending.ps1
+++ pipeline-scripts/Deploy-ALZHub-lzVending.ps1
@@ -3,6 +3,9 @@ param (
   [String]$ConnectivitySubscriptionId = "$($env:CONNECTIVITY_SUBSCRIPTION_ID)",
 
   [Parameter()]
+  [String]$Location = "$($env:LOCATION)",
+
+  [Parameter()]
   [String]$TemplateFile = "config\orchestration\hubPeeredSpoke\hubPeeredSpoke.bicep",
 
   [Parameter()]
@@ -16,6 +19,7 @@ param (
 $inputObject = @{
   DeploymentName        = 'alz-VWANDeploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
   TemplateFile          = $TemplateFile
+  Location              = $Location
   TemplateParameterFile = $TemplateParameterFile
   WhatIf                = $WhatIfEnabled
   Verbose               = $true
