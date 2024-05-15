818403d6f6ef4d40536288880445763bdb8c0c2b
Nikhil Bharathesh <nikhil.bharathesh_pa@hra.nhs.uk>
2024-05-15 10:27:04 +0100

    adding missing name property

--- pipeline-scripts/Deploy-ALZHub-lzVending.ps1
+++ pipeline-scripts/Deploy-ALZHub-lzVending.ps1
@@ -3,6 +3,9 @@ param (
   [String]$ConnectivitySubscriptionId = "$($env:CONNECTIVITY_SUBSCRIPTION_ID)",
 
   [Parameter()]
+  [String]$ConnectivityResourceGroup = "$($env:CONNECTIVITY_RESOURCE_GROUP)",
+
+  [Parameter()]
   [String]$TemplateFile = "config\orchestration\hubPeeredSpoke\hubPeeredSpoke.bicep",
 
   [Parameter()]
@@ -15,6 +18,7 @@ param (
 # Parameters necessary for deployment
 $inputObject = @{
   DeploymentName        = 'alz-VWANDeploy-{0}' -f ( -join (Get-Date -Format 'yyyyMMddTHHMMssffffZ')[0..63])
+  ResourceGroupName     = $ConnectivityResourceGroup
   TemplateFile          = $TemplateFile
   TemplateParameterFile = $TemplateParameterFile
   WhatIf                = $WhatIfEnabled
